516b53581f0a64fa81ae92a9a04a5f8c9b86f636
sasank <sasank@adobe.com>
2023-03-06 14:32:28 -0800

    Added search bar

--- franklin_scripts/scripts/utils.js
+++ franklin_scripts/scripts/utils.js
@@ -37,3 +37,14 @@ export const [setLibs, getLibs] = (() => {
     }, () => libs,
   ];
 })();
+
+/* eslint-disable no-restricted-syntax */
+export function createTag(name, attrs) {
+  const el = document.createElement(name);
+  if (typeof attrs === 'object') {
+    for (const [key, value] of Object.entries(attrs)) {
+      el.setAttribute(key, value);
+    }
+  }
+  return el;
+}
